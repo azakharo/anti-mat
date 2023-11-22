ac35275fab550572b9c3e7829579f4ae2057b6a8
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-11-22 19:01:30 +0300

    implement demo page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,20 +1,89 @@
-import {memo} from 'react';
-import {Box} from '@mui/material';
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  memo,
+  useCallback,
+  useState,
+} from 'react';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 
-const areaColor = '#e2deed';
+import {containsMat} from 'src/services/antimat';
+
+interface TextExample {
+  text: string;
+  hasMat: boolean;
+}
 
 const Main = () => {
+  const [input, setInput] = useState('');
+  const [texts, setTexts] = useState<TextExample[]>([]);
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+    evt => setInput(evt.target.value),
+    [setInput],
+  );
+
+  const handleSubmit: FormEventHandler = evt => {
+    evt.preventDefault();
+
+    const status = containsMat(input);
+
+    setTexts(prevTexts => [...prevTexts, {text: input, hasMat: status}]);
+    setInput('');
+  };
+
   return (
-    <Box
-      sx={{
-        height: '100vh',
-        '&> *': {
-          backgroundColor: areaColor,
-        },
-      }}
-    >
-      Hello World!
-    </Box>
+    <Container maxWidth="sm">
+      <Box my={2}>
+        <Typography variant="h5">
+          Проверка текста на нецензурную лексику
+        </Typography>
+
+        <Stack spacing={1} mt={2}>
+          {texts.map(({text, hasMat}) => (
+            <Box
+              py={1}
+              borderRadius={2}
+              display="flex"
+              alignItems="center"
+              gap={1}
+            >
+              {hasMat ? (
+                <ErrorOutlineOutlinedIcon sx={{fill: 'lightcoral'}} />
+              ) : (
+                <CheckCircleOutlineOutlinedIcon sx={{fill: 'lightgreen'}} />
+              )}
+              {text}
+            </Box>
+          ))}
+        </Stack>
+
+        <Box mt={2}>
+          <form onSubmit={handleSubmit}>
+            <Box display="flex" alignItems="center" gap={1}>
+              <TextField
+                value={input}
+                onChange={handleInputChange}
+                size="small"
+                placeholder="Введите текст"
+              />
+              <Button type="submit" variant="contained">
+                OK
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
